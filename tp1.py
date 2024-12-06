82a8fb49fa1a3d9abd144cc780840d8eecc34fd5
Maton Elisabeth <mato@insa-11283>
2024-12-06 13:43:42 +0100

    fin tp1

--- tp1.py
+++ tp1.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import StratifiedShuffleSplit, cross_val_score
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 
 
@@ -50,10 +52,68 @@ for train_index, test_index in sss.split(X, y):
 print(f"\nDimensions des données d'entrée X : {X.shape}")
 print(f"Dimensions des labels y : {y.shape}")
 
-#recherche de bons modèles
-#on commence avec le modèle type RandomForest :
+"""
+# I recherche de bons modèles
+# 1) on commence avec le modèle type RandomForest :
 rf_model = RandomForestClassifier()
 cv_scores_rf = cross_val_score(rf_model, X_train, y_train, cv=5).mean()
 print("Moyenne des scores de Validation croisée = ", cv_scores_rf)
 
 #on récupère les indices de qualité suivants pour chaque modèle : accuracy, classification_report, confusion_matrix
+rf_model.fit(X_train, y_train)
+rf_prediction = rf_model.predict(X_test)
+#acuracy
+ac_rf = accuracy_score(y_test, rf_prediction)
+#classification
+class_rf = classification_report(y_test, rf_prediction)
+#matrice de confusion
+mat_conf_rf = confusion_matrix(y_test, rf_prediction)
+
+print("Acuracy = ", ac_rf)
+print("classification report = ", class_rf)
+print("matrice de confusion = ", mat_conf_rf)
+
+
+# 2) on continue avec le modèle type AdaBoost :
+ab_model = AdaBoostClassifier()
+cv_scores_ab = cross_val_score(ab_model, X_train, y_train, cv=5).mean()
+print("Moyenne des scores de Validation croisée = ", cv_scores_ab)
+
+#on récupère les indices de qualité suivants pour chaque modèle : accuracy, classification_report, confusion_matrix
+ab_model.fit(X_train, y_train)
+ab_prediction = ab_model.predict(X_test)
+#acuracy
+ac_ab = accuracy_score(y_test, ab_prediction)
+#classification
+class_ab = classification_report(y_test, ab_prediction)
+#matrice de confusion
+mat_conf_ab = confusion_matrix(y_test, ab_prediction)
+
+print("Acuracy AdaBoost = ", ac_ab)
+print("classification report AdaBoost = ", class_ab)
+print("matrice de confusion AdaBoost = ", mat_conf_ab)
+
+
+
+# 2) on continue avec le modèle type GradientBoosting :
+
+gb_model = GradientBoostingClassifier()
+cv_scores_gb = cross_val_score(gb_model, X_train, y_train, cv=5).mean()
+print("Moyenne des scores de Validation croisée = ", cv_scores_gb)
+
+#on récupère les indices de qualité suivants pour chaque modèle : accuracy, classification_report, confusion_matrix
+gb_model.fit(X_train, y_train)
+gb_prediction = gb_model.predict(X_test)
+#acuracy
+ac_gb = accuracy_score(y_test, gb_prediction)
+#classification
+class_gb = classification_report(y_test, gb_prediction)
+#matrice de confusion
+mat_conf_gb = confusion_matrix(y_test, gb_prediction)
+
+print("Acuracy AdaBoost = ", ac_gb)
+print("classification report AdaBoost = ", class_gb)
+print("matrice de confusion AdaBoost = ", mat_conf_gb)
+
+"""
+
